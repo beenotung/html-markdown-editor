e0984513bd4d9eb37f2d890148230bc1f93c7a49
Beeno Tung <aabbcc1241@yahoo.com.hk>
2025-01-30 09:04:44 +0800

    feat: preserve bold in heading
    to make it easy to remove bold on all text

--- src/app.ts
+++ src/app.ts
@@ -8,15 +8,44 @@ let statusNode = querySelector('#status')
 let markdownEditor = querySelector<HTMLTextAreaElement>('#markdownEditor')
 let htmlEditor = querySelector('#htmlEditor')
 
+function markdown_to_html(markdown_text: string) {
+  let markdown_ast = markdown.block.parse(markdown_text)
+  let html_ast = toHast(markdown_ast)
+  let html_text = toHtml(html_ast)
+  return html_text
+}
+
+function html_to_markdown(html_text: string) {
+  let html_ast = html.html.parsef(html_text)
+  let md_ast = toMdast(html_ast)
+  let markdown = toMarkdown(md_ast)
+  return markdown
+}
+
 markdownEditor.oninput = () => {
-  let md_ast = markdown.block.parse(markdownEditor.value)
-  let html_ast = toHast(md_ast)
-  htmlEditor.innerHTML = toHtml(html_ast)
+  let html_text = markdown_to_html(markdownEditor.value)
+  htmlEditor.innerHTML = html_text
 }
 htmlEditor.oninput = () => {
-  let html_ast = html.html.parsef(htmlEditor.innerHTML)
-  let md_ast = toMdast(html_ast)
-  markdownEditor.value = toMarkdown(md_ast)
+  let markdown_text = html_to_markdown(htmlEditor.innerHTML)
+
+  let lines = markdown_text.split('\n')
+  let changed = false
+  lines.forEach((line, index) => {
+    // e.g. `### <span style="font-weight: normal;">Phase 1: Core Features</span>`
+    let match = line.match(
+      /^(#+) <span style="font-weight: normal;">(.*)<\/span>$/,
+    )
+    if (!match) return
+    let heading = match[1]
+    let title = match[2]
+    lines[index] = `${heading} ${title}`
+    changed = true
+  })
+  markdownEditor.value = lines.join('\n')
+  if (changed) {
+    htmlEditor.innerHTML = markdown_to_html(markdownEditor.value)
+  }
 }
 
 function calcSize() {
