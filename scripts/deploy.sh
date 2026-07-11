3d5a93b86c9af06681ee46676556400e97057266
Beeno Tung <aabbcc1241@yahoo.com.hk>
2026-07-12 05:30:20 +0800

    chore: inline katex for self-contained deployment - load it from the same domain instead of from cdn

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,6 +6,8 @@ npm run build
 
 mkdir -p public/dist
 cp dist/bundle.js public/dist/bundle.js
+mkdir -p public/lib/katex
+cp -r lib/katex/* public/lib/katex/
 cp index.html public/index.html
 
 npx surge public https://html-markdown-editor.surge.sh
